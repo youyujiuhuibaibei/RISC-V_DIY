55c2d12312955879bd7214a7f92bfe5b9551795e
zhr <1798069576@qq.com>
2024-11-22 17:15:05 +0800

    add exu

--- core/exu.v
+++ core/exu.v
@@ -1,7 +1,217 @@
 `include "../define.v"
 
 module exu (
-
+input [4:0] rd,
+input [31:0] imm_ext,
+input [31:0] rs1_data,
+input [31:0] rs2_data,
+input [6:0] opcode,
+input [2:0] funct3,
+input [6:0] funct7,
+input [31:0] PC,
+output [31:0] alu_out,
+output B_result
 );
-    
+
+wire [31:0] alu_add;
+wire [31:0] alu_sub;
+wire [31:0] alu_xor;
+wire [31:0] alu_or;
+wire [31:0] alu_and;
+wire [31:0] alu_sll;
+wire [31:0] alu_srl;
+wire [31:0] alu_sra;
+wire [31:0] alu_slt;
+wire [31:0] alu_sltu;
+
+wire [31:0] alu_addi;
+wire [31:0] alu_xori;
+wire [31:0] alu_ori;
+wire [31:0] alu_andi;
+wire [31:0] alu_slli;
+wire [31:0] alu_srli;
+wire [31:0] alu_srai;
+wire [31:0] alu_slti;
+wire [31:0] alu_sltiu;
+
+wire [31:0] alu_sb;
+wire [31:0] alu_sh;
+wire [31:0] alu_sw;
+
+wire [31:0] alu_B;
+wire [31:0] alu_eq;
+wire [31:0] alu_comp;
+wire [31:0] alu_comp_u;
+
+wire [31:0] alu_lui;
+wire [31:0] alu_auipc;
+
+reg [31:0] alu_r;
+reg B_result_r;
+
+assign alu_out = alu_r;
+assign B_result = B_result_r;
+
+// reg [31:0] op_1;//操作数1
+// reg [31:0] op_2;//操作数2
+// reg [31:0] op_3;//操作数3，用于分支跳转
+
+
+//                  A           L           U                  //////////////////////////////////////////////////////////////////////////
+//type_R
+assign alu_add = rs1_data + rs2_data;
+assign alu_sub = rs1_data - rs2_data;
+assign alu_xor = rs1_data ^ rs2_data;
+assign alu_or = rs1_data | rs2_data;
+assign alu_and = rs1_data & rs2_data;
+assign alu_sll = rs1_data << rs2_data[4:0];
+assign alu_srl = rs1_data >> rs2_data[4:0];
+assign alu_sra = (rs1_data >> rs2_data[4:0]) | ({32{rs1_data[31]}} & (~(32'hFFFFFFFF >> rs2_data[4:0])));//msb_extend
+assign alu_slt = (rs1_data[31] != rs2_data[31]) ? rs1_data[31] : (rs1_data < rs2_data);
+assign alu_sltu = (rs1_data < rs2_data) ? 1 : 0;
+
+//type_I
+assign alu_addi = rs1_data + imm_ext;
+assign alu_xori = rs1_data ^ imm_ext;
+assign alu_ori = rs1_data | imm_ext;
+assign alu_andi = rs1_data & imm_ext;
+assign alu_slli = rs1_data << imm_ext[4:0];
+assign alu_srli = rs1_data >> imm_ext[4:0];
+assign alu_srai = (rs1_data >> imm_ext[4:0]) | ({32{rs1_data[31]}} & (~(32'hFFFFFFFF >> imm_ext[4:0])));//msb_extend
+assign alu_slti = (rs1_data[31] != imm_ext[31]) ? rs1_data[31] : (rs1_data < imm_ext);
+assign alu_sltiu = (rs1_data < imm_ext) ? 1 : 0;
+
+assign alu_lb = rs1_data + imm_ext;
+assign alu_lh = rs1_data + imm_ext;
+assign alu_lw = rs1_data + imm_ext;
+assign alu_lbu = rs1_data + imm_ext;
+assign alu_lhu = rs1_data + imm_ext;
+
+//type_S
+assign alu_sb = rs1_data + imm_ext;
+assign alu_sh = rs1_data + imm_ext;
+assign alu_sw = rs1_data + imm_ext;
+
+//type_B
+assign alu_eq = (rs1_data == rs2_data) ? 1 : 0;//alu相等判决
+assign alu_comp = (rs1_data[31] != imm_ext[31]) ? rs1_data[31] : (rs1_data < imm_ext);//有符号比较，小于输出1，大于输出0
+assign alu_comp_u = (rs1_data < imm_ext) ? 1 : 0;//无符号比较，小于输出1，大于输出0
+assign alu_B = PC + (imm_ext << 2);
+
+//type_U
+assign alu_lui = imm_ext << 12;
+assign alu_auipc = PC + (imm_ext << 12);
+
+//type_J
+assign alu_jal = PC + (imm_ext << 2);
+assign alu_jalr = PC + rs1_data + (imm_ext << 2);
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//                   ALUop                     ///////////////////////////////////////////////////////////////////////////////////////////////
+//判断使用哪一个ALU结果
+always @(*) begin
+    case (opcode)
+        `opcode_R:begin//R型指令
+            case (funct3)
+                `funct3_add:begin
+                    case (funct7)
+                        `funct7_add: alu_r <= alu_add;
+                        `funct7_sub: alu_r <= alu_sub;
+                        default: alu_r <= alu_add;
+                    endcase
+                end 
+                `funct3_xor: alu_r <= alu_xor;
+                `funct3_or: alu_r <= alu_or;
+                `funct3_and: alu_r <= alu_and;
+                `funct3_sll: alu_r <= alu_sll;
+                `funct3_srl:begin
+                    case (funct7)
+                        `funct7_srl: alu_r <= alu_srl;
+                        `funct7_sra: alu_r <= alu_sra;
+                        default: alu_r <= alu_srl;
+                    endcase
+                end
+                `funct3_slt: alu_r <= alu_slt;
+                `funct3_sltu: alu_r <= alu_sltu;
+                default: alu_r <= 0;
+            endcase
+        end 
+        `opcode_I_lg:begin//I型算术逻辑指令
+            case (funct3)
+                `funct3_addi: alu_r <= alu_addi;
+                `funct3_xori: alu_r <= alu_xori; 
+                `funct3_ori: alu_r <= alu_ori;
+                `funct3_andi: alu_r <= alu_andi; 
+                `funct3_slli: alu_r <= alu_slli;
+                `funct3_srli: alu_r <= alu_srli; 
+                `funct3_srai: alu_r <= alu_srai;
+                `funct3_slti: alu_r <= alu_slti; 
+                `funct3_sltiu: alu_r <= alu_sltiu;
+                default: alu_r <= alu_addi;
+            endcase
+        end 
+        `opcode_I_ld:begin//load指令
+            case (funct3)
+                `funct3_lb: alu_r <= alu_lb;
+                `funct3_lh: alu_r <= alu_lh;
+                `funct3_lw: alu_r <= alu_lw;
+                `funct3_lbu: alu_r <= alu_lbu;
+                `funct3_lhu: alu_r <= alu_lhu;
+                default: alu_r <= alu_lb;
+            endcase
+        end 
+        `opcode_S:begin//S型指令
+            case (funct3)
+                `funct3_sb: alu_r <= alu_sb;
+                `funct3_sh: alu_r <= alu_sh;
+                `funct3_sw: alu_r <= alu_sw;
+                default: alu_r <= alu_sb;
+            endcase
+        end 
+        `opcode_B:begin//B型指令
+            case (funct3)
+                `funct3_beq: alu_r <= alu_B;
+                `funct3_beq: alu_r <= alu_B;
+                `funct3_beq: alu_r <= alu_B;
+                `funct3_beq: alu_r <= alu_B;
+                `funct3_beq: alu_r <= alu_B;
+                `funct3_beq: alu_r <= alu_B;
+                default: alu_r <= alu_B;
+            endcase
+        end 
+        `opcode_U_lui:begin
+            alu_r <= alu_lui;
+        end 
+        `opcode_U_auipc:begin
+            alu_r <= alu_auipc;
+        end 
+        `opcode_J_jal:begin
+            alu_r <= alu_jal;
+        end 
+        `opcode_J_jalr:begin
+            alu_r <= alu_jalr;
+        end 
+        default: alu_r <= alu_add;
+    endcase
+end
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//分支跳转判决输出
+always @(*) begin
+    if (opcode==`opcode_B) begin
+        case (funct3)
+            `funct3_beq: B_result_r <= alu_eq;
+            `funct3_bne: B_result_r <= ~alu_eq;
+            `funct3_blt: B_result_r <= alu_comp;
+            `funct3_bge: B_result_r <= ~alu_comp;
+            `funct3_bltu: B_result_r <= alu_comp_u;
+            `funct3_bgeu: B_result_r <= ~alu_comp_u;
+            default: B_result_r <= 0;//1跳转，0不跳转
+        endcase
+    end else begin
+        B_result_r <= 0;//1跳转，0不跳转
+    end
+end
+
+
 endmodule
--- define.v
+++ define.v
@@ -2,120 +2,120 @@
 //type_R
 `define opcode_R        0110011
 
-`define funct3_add      0x0
-`define funct3_sub      0x0
-`define funct3_xor      0x4
-`define funct3_or       0x6
-`define funct3_and      0x7
-`define funct3_sll      0x1
-`define funct3_srl      0x5
-`define funct3_sra      0x5
-`define funct3_slt      0x2
-`define funct3_sltu     0x3
+`define funct3_add      3'h0 //f3一样
+`define funct3_sub      3'h0 //
+`define funct3_xor      3'h4
+`define funct3_or       3'h6
+`define funct3_and      3'h7
+`define funct3_sll      3'h1
+`define funct3_srl      3'h5 //
+`define funct3_sra      3'h5 //
+`define funct3_slt      3'h2
+`define funct3_sltu     3'h3
 
-`define funct7_add      0x00
-`define funct7_sub      0x20
-`define funct7_xor      0x00
-`define funct7_or       0x00
-`define funct7_and      0x00
-`define funct7_sll      0x00
-`define funct7_srl      0x00
-`define funct7_sra      0x20
-`define funct7_slt      0x00
-`define funct7_sltu     0x00
+`define funct7_add      7'h00
+`define funct7_sub      7'h20    //
+`define funct7_xor      7'h00
+`define funct7_or       7'h00
+`define funct7_and      7'h00
+`define funct7_sll      7'h00
+`define funct7_srl      7'h00
+`define funct7_sra      7'h20    //
+`define funct7_slt      7'h00
+`define funct7_sltu     7'h00
 
-`define ALUopcode_add   0x0
-`define ALUopcode_sub   0x1
-`define ALUopcode_xor   0x2
-`define ALUopcode_or    0x3
-`define ALUopcode_and   0x4
-`define ALUopcode_sll   0x5
-`define ALUopcode_srl   0x6
-`define ALUopcode_sra   0x7
-`define ALUopcode_slt   0x8
-`define ALUopcode_sltu  0x9
+// `define ALUopcode_add   0x0
+// `define ALUopcode_sub   0x1
+// `define ALUopcode_xor   0x2
+// `define ALUopcode_or    0x3
+// `define ALUopcode_and   0x4
+// `define ALUopcode_sll   0x5
+// `define ALUopcode_srl   0x6
+// `define ALUopcode_sra   0x7
+// `define ALUopcode_slt   0x8
+// `define ALUopcode_sltu  0x9
 
 
 //type_I
 `define opcode_I_lg     0010011
 
-`define funct3_addi     0x0
-`define funct3_xori     0x4
-`define funct3_ori      0x6
-`define funct3_andi     0x7
-`define funct3_slli     0x1
-`define funct3_srli     0x5
-`define funct3_srai     0x5
-`define funct3_slti     0x2
-`define funct3_sltiu    0x3
+`define funct3_addi     3'h0
+`define funct3_xori     3'h4
+`define funct3_ori      3'h6
+`define funct3_andi     3'h7
+`define funct3_slli     3'h1
+`define funct3_srli     3'h5
+`define funct3_srai     3'h5
+`define funct3_slti     3'h2
+`define funct3_sltiu    3'h3
 
-`define ALUopcode_addi  0xa
-`define ALUopcode_xori  0xb
-`define ALUopcode_ori   0xc
-`define ALUopcode_andi  0xd
-`define ALUopcode_slli  0xe
-`define ALUopcode_srli  0xf
-`define ALUopcode_srai  0x10
-`define ALUopcode_slti  0x11
-`define ALUopcode_sltui 0x12
+// `define ALUopcode_addi  0xa
+// `define ALUopcode_xori  0xb
+// `define ALUopcode_ori   0xc
+// `define ALUopcode_andi  0xd
+// `define ALUopcode_slli  0xe
+// `define ALUopcode_srli  0xf
+// `define ALUopcode_srai  0x10
+// `define ALUopcode_slti  0x11
+// `define ALUopcode_sltui 0x12
 
 `define opcode_I_ld     0000011
 
-`define funct3_lb       0x0
-`define funct3_lh       0x1
-`define funct3_lw       0x2
-`define funct3_lbu      0x4
-`define funct3_lhu      0x5
+`define funct3_lb       3'h0
+`define funct3_lh       3'h1
+`define funct3_lw       3'h2
+`define funct3_lbu      3'h4
+`define funct3_lhu      3'h5
 
-`define ALUopcode_lb       0x13
-`define ALUopcode_lh       0x14
-`define ALUopcode_lw       0x15
-`define ALUopcode_lbu      0x16
-`define ALUopcode_lhu      0x17
+// `define ALUopcode_lb       0x13
+// `define ALUopcode_lh       0x14
+// `define ALUopcode_lw       0x15
+// `define ALUopcode_lbu      0x16
+// `define ALUopcode_lhu      0x17
 
 //type_S
 `define opcode_S        0100011
 
-`define funct3_sb       0x0
-`define funct3_sh       0x1
-`define funct3_sw       0x2
+`define funct3_sb       3'h0
+`define funct3_sh       3'h1
+`define funct3_sw       3'h2
 
-`define ALUopcode_sb       0x18
-`define ALUopcode_sh       0x19
-`define ALUopcode_sw       0x20
+// `define ALUopcode_sb       0x18
+// `define ALUopcode_sh       0x19
+// `define ALUopcode_sw       0x20
 
 //type_B
 `define opcode_B        1100011
 
-`define funct3_beq      0x0
-`define funct3_bne      0x1
-`define funct3_blt      0x4
-`define funct3_bge      0x5
-`define funct3_bltu     0x6
-`define funct3_bgeu     0x7
+`define funct3_beq      3'h0
+`define funct3_bne      3'h1
+`define funct3_blt      3'h4
+`define funct3_bge      3'h5
+`define funct3_bltu     3'h6
+`define funct3_bgeu     3'h7
 
-`define ALUopcode_beq      0x2a
-`define ALUopcode_bne      0x2b
-`define ALUopcode_blt      0x2c
-`define ALUopcode_bge      0x2d
-`define ALUopcode_bltu     0x2e
-`define ALUopcode_bgeu     0x2f
+// `define ALUopcode_beq      0x2a
+// `define ALUopcode_bne      0x2b
+// `define ALUopcode_blt      0x2c
+// `define ALUopcode_bge      0x2d
+// `define ALUopcode_bltu     0x2e
+// `define ALUopcode_bgeu     0x2f
 
 //type_U
 `define opcode_U_lui    0110111
 
-`define ALUopcode_U_lui     0x30
+//`define ALUopcode_U_lui     0x30
 
 `define opcode_U_auipc  0010111
 
-`define ALUopcode_U_auipc     0x31
+//`define ALUopcode_U_auipc     0x31
 
 //type_J
 `define opcode_J_jal    1101111
 
-`define ALUopcode_J_jal     0x32
+//`define ALUopcode_J_jal     0x32
 
 `define opcode_J_jalr   1100111
 
 
-`define ALUopcode_J_jalr     0x33
+//`define ALUopcode_J_jalr     0x33
